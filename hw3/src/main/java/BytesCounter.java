6ae4eda37736c1f1656b581af4d69505e8ceaa72
Anastasiia Iurshina <Anastasiia_Iurshina@epam.com>
2015-10-22 12:14:56 +0300

    Homework 3 in progress

--- hw3/src/main/java/BytesCounter.java
+++ hw3/src/main/java/BytesCounter.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -36,9 +37,6 @@ public class BytesCounter extends Configured implements Tool {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        FileOutputFormat.setCompressOutput(job, true);
-        FileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
-
         job.setMapperClass(CounterMapper.class);
         job.setCombinerClass(CounterCombiner.class);
         job.setReducerClass(CounterReducer.class);
@@ -46,6 +44,10 @@ public class BytesCounter extends Configured implements Tool {
         job.setOutputKeyClass(Text.class);
         job.setMapOutputValueClass(LongPair.class);
         job.setOutputValueClass(LongDoublePair.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        //TODO: doesn't work?
+        FileOutputFormat.setCompressOutput(job, true);
+        FileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
--- hw3/src/main/java/values/LongPair.java
+++ hw3/src/main/java/values/LongPair.java
@@ -1,7 +1,6 @@
 package values;
 
 import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.WritableComparator;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -54,22 +53,6 @@ public class LongPair implements WritableComparable {
         } else {
             return false;
         }
-    }
-
-    public static class Comparator extends WritableComparator {
-
-        public Comparator() {
-            super(LongPair.class);
-        }
-
-        public int compare(byte[] b1, int s1, int l1,
-                           byte[] b2, int s2, int l2) {
-            return compareBytes(b1, s1, l1, b2, s2, l2);
-        }
-    }
-
-    static {
-        WritableComparator.define(LongPair.class, new Comparator());
     }
 
     public int compareTo(Object that) {
